8a35ecdbebb2fe63b6964081e61909a74cc912fd
Tadanori TERUYA <tadanori.teruya@gmail.com>
2011-05-31 09:20:49 +0900

    untabify

--- number-theory/modpow.scm
+++ number-theory/modpow.scm
@@ -3,13 +3,13 @@
     (remainder (* x y) m))
   (let ((len (integer-length e)))
     (let iter ((result 1)
-	       (x x)
-	       (i 0))
+           (x x)
+           (i 0))
       (if (= i len)
-	  result
-	  (if (logbit? i e)
-	      (iter (modmul result x) (modmul x x) (+ i 1))
-	      (iter result (modmul x x) (+ i 1)))))))
+      result
+      (if (logbit? i e)
+          (iter (modmul result x) (modmul x x) (+ i 1))
+          (iter result (modmul x x) (+ i 1)))))))
 
 (modpow-LSB 1234 5678 91011)
 
@@ -20,14 +20,14 @@
     (remainder (* x x) m))
   (let ((len (integer-length e)))
     (let iter ((result x)
-	       (i (- len 2)))
+           (i (- len 2)))
       (if (< i 0)
-	  result
-	  (let ((sq (modsq result)))
-	    (iter
-	     (if (logbit? i e)
-		 (modmul sq x)
-		 sq)
-	     (- i 1)))))))
+      result
+      (let ((sq (modsq result)))
+        (iter
+         (if (logbit? i e)
+             (modmul sq x)
+             sq)
+         (- i 1)))))))
 
 (modpow-MSB 1234 5678 91011)
